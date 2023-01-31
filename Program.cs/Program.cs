f95196f823986e171a16b12c771549556ef3ad3a
kattbcc17 <kattbcc17@byui.edu>
2023-01-30 17:01:08 -0700

    Journal App in Visual Studio

---
+++ Program.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JournalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal myJournal = new Journal();
+            myJournal.Run();
+
+        }
+    }
+}
